108afc7d85bdd08072b7780ab4cca23ea39f5114
jrwebbjr <webbjr@zohomail.com>
2022-07-06 11:18:21 -0500

    added svg graphics, arranged post its

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    backgroundImage: {
+      'woodgrain': "url('/public/woodgrain.jpg')",
+      'vine': "url('/public/PNG/voodoo_pumpkin-vine-01.png')",
+      'skull': "url('/public/PNG/voodoo_skull-01.png')",
+    },
+    fontFamily: {
+      'voodoo' : ['Voodoo', 'sans'],
+      'shs' : ['Stolen-Hand-Sketches', 'sans'],
+    },
     colors: {
       'pi-yellow' : '#fdfd96',
       'pi-purple' : '#fd96fd',

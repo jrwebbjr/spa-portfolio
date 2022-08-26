8a81477e1d4471053945a93e65741538c827fe47
jrwebbjr <webbjr@zohomail.com>
2022-08-26 11:08:16 -0500

    updated large screen projects, working on pop up of detailed project info

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,27 +5,35 @@ export default function Projects(){
         <div>
             <p className='flex justify-center items-center font-voodoo text-[26px] md:text-5xl md:p-10'>
                 <ul className='flex flex-col justify-evenly items-center md:gap-8'>
-                  <li className='flex justify-evenly items-center hover:font-voodoo-bold hover:text-blue-800'>
-                    Apply
-                    <a href="https://github.com/jrwebbjr/interview-tracker-app-backend">
-                    <div className='hover:fill-red-700'>
-                        <GithubIcon />
+                  <li className='flex justify-evenly items-center hover:font-voodoo-bold hover:text-blue-800 hover:text-4xl lg:hover:text-7xl'>
+                  <a href="https://google.com" target="blank">Apply</a>
+                    <a className='hover:text-blue-800' href="https://github.com/jrwebbjr/interview-tracker-app-backend">
+                    <div className='hover:text-red-700'>
+                      <GithubIcon />
                     </div>
                     </a>
                   </li>
-                  <li className='flex justify-evenly items-center hover:font-voodoo-bold hover:text-blue-800'>
-                    The Healing Chi
-                    <a href="https://github.com/Nobuns-inc/Yoga-App">
-                    <div className='hover:fill-red-700'>
+                  <li className='flex justify-evenly items-center hover:font-voodoo-bold hover:text-blue-800 hover:text-4xl lg:hover:text-7xl'>
+                    <a href="https://google.com" target="blank">Retrospective</a>
+                    <a className='hover:text-blue-800' href="https://github.com/jrwebbjr/Retrospective">
+                      <div className='hover:text-red-700'>
                         <GithubIcon />
-                    </div>
+                      </div>
                     </a>
                   </li>
-                  <li className='flex justify-evenly items-center hover:font-voodoo-bold hover:text-blue-800'>
-                    Retrospective
-                    <a href="https://github.com/jrwebbjr/Retrospective">
-                    <div className='hover:fill-red-700'>
+                  <li className='flex justify-evenly items-center hover:font-voodoo-bold hover:text-blue-800 hover:text-4xl lg:hover:text-7xl'>
+                    <a href="https://google.com" target="blank">Emoji Matchup</a>
+                    <a className='hover:text-blue-800' href="https://github.com/jrwebbjr/Emoji-Matchup" target="blank">
+                      <div className='hover:text-red-700'>
                         <GithubIcon />
+                      </div>
+                    </a>
+                  </li>
+                  <li className='flex justify-evenly items-center hover:font-voodoo-bold hover:text-blue-800 hover:text-4xl lg:hover:text-7xl'>
+                    <a href="https://google.com" target="blank">The Healing Chi</a>
+                    <a classname="hover:text-blue-800" href="https://github.com/Nobuns-inc/Yoga-App">
+                    <div className='hover:text-red-700'>
+                      <GithubIcon />
                     </div>
                     </a>
                   </li>
